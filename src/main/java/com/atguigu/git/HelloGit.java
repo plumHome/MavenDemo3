4d8a7e62eaa2bca14adba120d485bcdc76de2621
srwang <srwang@guigu.com>
2021-05-15 14:30:37 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,14 @@
+package com.atguigu.git;
+
+/**
+ * @Title: HelloGit
+ * @Description:
+ * @Auther:
+ * @Version: 1.0
+ * @create 2021/5/15 11:58
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
