204d10f45389f7e61dad9dd16c2689ca6bb3da0e
srwang <srwang@guigu.com>
2021-05-15 14:32:26 +0800

    third commit

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -11,6 +11,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
+        System.out.println("hello git3");
 
     }
 }
