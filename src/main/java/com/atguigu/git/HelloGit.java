3417fab0d72e4e3ee96f30562a5dfb549d736bd3
srwang <srwang@guigu.com>
2021-05-15 14:45:36 +0800

    five commit

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -13,6 +13,7 @@ public class HelloGit {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hot-fix");
 
     }
 }
